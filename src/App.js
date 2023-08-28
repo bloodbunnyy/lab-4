de77018eb30a3a338d71ad82f18f9639ea99ae4f
Jess R <jnryan7@hotmail.com>
2023-08-28 20:08:14 +1000

    made changes to app.js and list.js for lab exercises

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import List from './components/List';
 import './App.css';
 
+const tasks = [
+    { description: 'Learn React', completed: true },
+    { description: 'Learn JSX', completed: false },
+    { description: 'Build a React App', copleted: false}
+];
+
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
-        </div>
+            <List heading='My Tasks' tasks={tasks}/> 
+        </div> 
     );
 }
 
